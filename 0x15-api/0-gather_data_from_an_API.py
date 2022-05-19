be52b0ce05269de9eacc2cccce1dca02fe417302
DaboRestrepo <daborestrepo@gmail.com>
2022-05-19 17:43:41 -0500

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""""""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    userId = argv[1]
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(userId))
+
+    employee = user.json().get('name')
+    completed = 0
+    totalTask = 0
+
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos')
+
+    for do in todo.json():
+        if do.get('userId') == int(userId):
+            totalTask += 1
+            if do.get('completed'):
+                completed += 1
+    print('Employee {} is done with tasks({}/{})'.format(employee, completed,
+                                                         totalTask))
+    for title in todo.json():
+        if title.get('userId') == int(userId):
+            if title.get('completed'):
+                print('\t{} '.format(title.get('title')))
