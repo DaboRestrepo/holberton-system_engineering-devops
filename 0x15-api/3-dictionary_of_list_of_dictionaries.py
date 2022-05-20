282259c6c24fcd18391236dd2c1c4197b8856020
DaboRestrepo <daborestrepo@gmail.com>
2022-05-19 19:53:13 -0500

    Import all the json data

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module export all the employee data in json format"""
+import json
+import requests
+
+
+if __name__ == "__main__":
+    user = requests.get('https://jsonplaceholder.typicode.com/users/').json()
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+
+    total_usr = {}
+    for usr in user:
+        id_list = []
+        for do in todo:
+            userId = do.get('userId')
+            if usr.get('id') == userId:
+                task_dict = {"task": do.get('title'),
+                             "completed": do.get('completed'),
+                             "username": usr.get('username')}
+                id_list.append(task_dict)
+            total_usr[userId] = id_list
+
+    file = 'todo_all_employees.json'
+    with open(file, mode='w') as f:
+        json.dump(total_usr, f)
