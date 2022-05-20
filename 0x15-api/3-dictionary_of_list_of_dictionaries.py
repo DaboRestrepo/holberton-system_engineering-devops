9ef916933a8af49ef5080db7edd8608d63856baa
DaboRestrepo <daborestrepo@gmail.com>
2022-05-19 20:16:39 -0500

    Fix the key: value obj

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -12,13 +12,12 @@ if __name__ == "__main__":
     for usr in user:
         id_list = []
         for do in todo:
-            userId = do.get('userId')
-            if usr.get('id') == userId:
+            if do.get('userId') == usr.get('id'):
                 task_dict = {"username": usr.get('username'),
                              "task": do.get('title'),
                              "completed": do.get('completed')}
                 id_list.append(task_dict)
-            total_usr[userId] = id_list
+        total_usr[usr.get('id')] = id_list
 
     file = 'todo_all_employees.json'
     with open(file, mode='w') as f:
